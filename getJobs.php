a02d534f704b9f80182fa78149922a1965497b8f
diaz2691 <jodiaz@csumb.edu>
2015-05-30 18:32:28 -0700

    Fixed ajax of jobListing.php

--- getJobs.php
+++ getJobs.php
@@ -9,7 +9,6 @@
 		$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		return $dbConn;
 	}
-
 	$dbConn= getConnection();
 	$sql = "SElECT * FROM Job where jobZip = :zip";
 	$namedParameters = array();
--- jobListing.php
+++ jobListing.php
@@ -27,12 +27,12 @@
   <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
   <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
   <script>
-				function getJobList(score) {  
+				function getJobList() {  
 					$.ajax({
 						type: "get",
 						url: "getJobs.php",
 						dataType: "json",
-						data:{"zip":$('zipcode').val()},
+						data:{"zip":$('#zipcode').val()},
 						success: function(data,status) {
 							$('filtersDiv').html("");
 							for(i = 0 ;i < data.length; i++)
