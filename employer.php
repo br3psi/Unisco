346c4e34f68ee4032240a4ee1717508a940e9a2f
diaz2691 <jodiaz@csumb.edu>
2015-06-08 15:57:59 -0700

    Added employer.php and employerSheet.css for employer side of the website

---
+++ employer.php
@@ -0,0 +1,60 @@
+<?php
+
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+       Remove this if you use the .htaccess -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>unisco - login</title>
+  <meta name="description" content="">
+  <meta name="author" content="Jose">
+
+  <meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+  <!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+<!-- <script src="js/___jquery-2.1.3.js"></script>
+
+<link rel="stylesheet" type="text/css" href="prefixed.css">
+<link href="bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+
+<script src="js/popup.js"></script> -->
+<link rel="stylesheet" type="text/css" href="css/employerSheet.css">
+</head>
+<body >
+
+	<nav>
+		<div>
+			<span>My Jobs</span>
+
+		</div>
+	</nav>
+
+	<div id="leftSideMenu">
+		<div>
+			<span>Interest Level</span>
+			<br/>
+			<input type="checkbox" value="yes">Yes</input>
+			<br/>
+			<input type="checkbox" value="no">No</input>
+			<br/>
+			<input type="checkbox" value="maybe">Maybe</input>
+		</div>
+
+	</div>
+
+	<div id="applicants">
+
+	</div>
+
+</body>
+</html>
