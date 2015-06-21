3977cfd31bd63f1be848765579c22022d6beb06d
diaz2691 <jodiaz@csumb.edu>
2015-06-21 01:02:27 -0700

    Saving dat from basic application

--- resumeApplication.php
+++ resumeApplication.php
@@ -391,9 +391,9 @@ if(isset($_POST['firstName']))
 						<button id="finishingApplication" type="submit" class="btn btn-primary well-lg" href="submitResume.php">Upload Resume</button>
 					</form>
 					<span id="orSpan" style = 'margin: 50px;'>OR</span>
-					<form method="POST" >
+					
 					<button id="finishingApplication" type="submit" class="btn btn-primary well-lg" >Finish Application</button>
-					</form>
+					
 				</div>		
 		</form>
 	</div>
