082d52adb1f4560468e5deb18372bfc5bb03f7ff
Monse Hernandez <mhernandezbucio@csumb.edu>
2015-06-28 16:40:36 -0700

    changed application.php

--- application.php
+++ application.php
@@ -354,7 +354,7 @@ function getConnection()
 									<div class="md-3 input-group">
 										<span class="form-control">If not, who did? </span>
 										<label class="input-group-addon">
-											<input  type="text" name="whoCompletedApplication">
+											<input type="text" name="whoCompletedApplication">
 										</label>
 									</div>
 								</div>
