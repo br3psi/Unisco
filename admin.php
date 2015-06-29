c4dd454981b13a5caa5414c52d6ad81435cdc7fb
diaz2691 <jodiaz@csumb.edu>
2015-06-29 15:22:01 -0700

    Add getEmployers.php to display active employers

--- admin.php
+++ admin.php
@@ -20,7 +20,9 @@
   <!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
   <link rel="shortcut icon" href="/favicon.ico">
   <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-
+  
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script src="js/admin.js"></script>
 </head>
 <body >
