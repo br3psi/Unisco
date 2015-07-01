953a746e2feb6071da008f8468e7554cd9265b0c
diaz2691 <jodiaz@csumb.edu>
2015-06-30 22:11:43 -0700

    adding linkedin button

--- resumeApplication.php
+++ resumeApplication.php
@@ -94,6 +94,10 @@ if(!empty($_POST['firstName']))
   <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
   <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 
+<script type="text/javascript" src="//platform.linkedin.com/in.js">
+    api_key:   75mu9f6oip0v45
+   
+</script>
 </head>
 <body style='height:auto !important;'>
 	<div id = 'header'>
@@ -207,7 +211,7 @@ if(!empty($_POST['firstName']))
 						</div>
 						<div class = 'col-md-4'>
 							<div class="input-group">
-								<input class = "btn btn-info" type="button" name="linkedIn" value="connect to linkedIn"/>	
+								<script type="in/Login"></script>	
 							</div>
 						</div>
 									
