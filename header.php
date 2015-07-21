e2b905ad0de8adf94196f66db186d110e65879a4
diaz2691 <jodiaz@csumb.edu>
2015-07-21 11:31:11 -0700

    Added bank icon

--- header.php
+++ header.php
@@ -73,7 +73,7 @@
 
 		<li class='option'>
 			<div style = 'padding:0;margin:0;'>
-				<a href="" class="btn btn-link" data-placement="bottom" data-toggle="tooltip" title="bank"><i class = 'icon-bank'></i></a>
+				<a href="index.php" class="btn btn-link" data-placement="bottom" data-toggle="tooltip" title="bank"><i class = 'icon-bank'></i></a>
 			</div>
 		</li>
 
