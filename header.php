fa02ea05b34f97a4c51027303dfd41cb6136e74b
diaz2691 <jodiaz@csumb.edu>
2015-07-21 11:28:32 -0700

    Added bank icon

--- header.php
+++ header.php
@@ -73,7 +73,7 @@
 
 		<li class='option'>
 			<div style = 'padding:0;margin:0;'>
-				<a href="" class="btn btn-link" data-placement="bottom" data-toggle="tooltip" title="bank"><span class = 'glyphicon glyphicon-piggy-bank' aria-hidden="true"></span></a>
+				<a href="" class="btn btn-link" data-placement="bottom" data-toggle="tooltip" title="bank"><i class = 'icon-bank'></i></a>
 			</div>
 		</li>
 
