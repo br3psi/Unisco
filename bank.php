874ab8758178f7e76ecb0586aa71db03167effd4
diaz2691 <jodiaz@csumb.edu>
2015-07-22 00:56:57 -0700

    Displaying applied to jobsin bank.php

--- bank.php
+++ bank.php
@@ -69,7 +69,7 @@
 
       foreach ($result as $job)
       {
-        echo "<td><b>" . $job[jobCompany] . "</b></td><td><b> " . job[jobPosition] . "</b></td>";
+        echo "<td><b>" . $job[jobCompany] . "</b></td><td><b> " . $job[jobPosition] . "</b></td>";
       }
 
     ?>
