6cc390036c82a8f59936ffcde43c3413edfd9463
Monse Hernandez <mhernandezbucio@csumb.edu>
2015-08-13 23:42:55 -0700

    Fixed code sent - generates a random one

--- js/sendCode.php
+++ js/sendCode.php
@@ -12,7 +12,7 @@ function generateCode($length){
 }
 
 
-$codeNum = generateCode(4);
+$codeNum = generateCode(5);
 require "twilio-php-master/Services/Twilio.php";
 	$AccountSid = "AC4991f00911beb00578efd8b8355fdc7d";
 	$AuthToken = "b605b8121c246b4b64fe407255f50528";
