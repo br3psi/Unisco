e28195a1521c3725d687edd91cbeaf4d8bb2b8db
Monse Hernandez <mhernandezbucio@csumb.edu>
2015-10-05 15:31:03 -0700

    Fixed send thank you msgs

--- jobListing.php
+++ jobListing.php
@@ -414,7 +414,7 @@ else
 
 				function setApply(jobId,com,pos)
 				{
-					sendCode(<?php $_POST['phone']?>);	
+					sendCode(<?php echo $_POST['phone']?>);	
 					$('#job-confirmation .success-check2').css('stroke-dashoffset',1000);
 					$('.success-circle').css({'pointer-events':'all'});
 					
