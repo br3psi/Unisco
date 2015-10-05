eb8628b8ead27e43a3b9934216379061b2e52164
Monse Hernandez <mhernandezbucio@csumb.edu>
2015-10-05 15:08:40 -0700

    Fixed send thank you msgs

--- jobListing.php
+++ jobListing.php
@@ -507,7 +507,7 @@ else
 		$.ajax({
 		type:"POST",
 		url: "sendThankyouMsg.php",
-		data:{"num":$_SESSION['phone'] },
+		data:{"num":<?php $_SESSION['phone'] ?>},
 		success: function(data,status){
 		
 	  	}
