41b6281210542a73b2e28ff6d5ee30f98767c02a
Monse Hernandez <mhernandezbucio@csumb.edu>
2015-10-05 15:20:23 -0700

    Fixed send thank you msgs

--- thankYouMessage.php
+++ thankYouMessage.php
@@ -1,9 +1,9 @@
 <?php
 require 'php/dbConnection.php';
 
-	$dbConn = getConnection();
+$dbConn = getConnection();
 $codeNum = rand(1000, 9999);
-require "twilio-php-master/Services/Twilio.php";
+require "js/twilio-php-master/Services/Twilio.php";
 	$AccountSid = "AC4991f00911beb00578efd8b8355fdc7d";
 	$AuthToken = "b605b8121c246b4b64fe407255f50528";
 	
