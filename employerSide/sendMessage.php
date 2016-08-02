33e954a80bb0ccdfc3ce1e70980cc607aed78c6b
diaz2691 <jodiaz@csumb.edu>
2016-08-01 17:11:49 -0700

    Fixed sending message

--- employerSide/sendMessage.php
+++ employerSide/sendMessage.php
@@ -6,7 +6,7 @@ require "../twilio-php-master/Services/Twilio.php";
 	$client = new Services_Twilio($AccountSid, $AuthToken);
 	
 	$message = $client->account->messages->create(array(
-				"From" => "8315851661",
+				"From" => "Unisco",
 				"To" => "+18312934153",
 				"Body" => $_POST['message'],
 ));
